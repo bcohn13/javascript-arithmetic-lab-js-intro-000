d3524d673480d684119d45bed76311da829d8fe5
Benjamin Cohn <cohnbj13@gmail.com>
2020-03-16 23:23:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ function add(n1,n2) {
   return n1+n2
 }
 
+function subtract(n1,n2) {
+  return n1-n2
+}
+
+function multiply(n1,n2) {
+  return n1*n2
+}
+
+function divide(n1,n2) {
+  return n1/n2
+}
+
 function makeInt(string) {
   return parseInt(string,10)
 }
